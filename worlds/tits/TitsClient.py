91ec6d908d76737d4ca36f0afabd7b6ab2cc3cbe
digiholic <digikun@gmail.com>
2025-03-27 22:45:58 -0600

    Now invokes event on item receive

--- worlds/tits/TitsClient.py
+++ worlds/tits/TitsClient.py
@@ -9,10 +9,6 @@ import websockets
 
 from CommonClient import CommonContext, gui_enabled, get_base_parser, server_loop, ClientCommandProcessor
 
-
-# webserver imports
-import urllib.parse
-
 from Utils import async_start
 
 logger = logging.getLogger("Client")
@@ -26,46 +22,15 @@ class TitsCommandProcessor(ClientCommandProcessor):
         super().__init__(ctx)
         self.ctx = ctx
 
-    def _cmd_tits(self):
-        logger.info("Checking T.I.T.S. status...")
-        async_start(self.ctx.connect_to_api(), name="connecting")
+    def _cmd_tits_connect(self):
+        async_start(self.ctx.connect_to_api(), name="connecting to tits")
 
+    def _cmd_tits_status(self):
+        self.ctx.tits_status()
 
-class TitsGameContext(CommonContext):
-    from kvui import GameManager
-    game = ""
-    httpServer_task: typing.Optional["asyncio.Task[None]"] = None
-    tags = CommonContext.tags
-    command_processor = TitsCommandProcessor
-    titSocket = None
-
-    def __init__(self, server_address, password):
-        super().__init__(server_address, password)
-
-
-    async def connect_to_api(self):
-        try:
-            self.titSocket = await websockets.connect("ws://localhost", port=42069, ping_timeout=None, ping_interval=None,
-                                          ssl=None, max_size=self.max_size)
-            self.get_trigger_list();
-
-        except Exception:
-            logger.info("Unable to connect. Ensure T.I.T.S. is running and API is enabled")
-
-    def get_trigger_list(self):
-        if self.titSocket is not None:
-            self.titSocket.send(request_trigger_list("0"))
-            result = self.titSocket.recv()
-            logger.info(result)
-            self.titSocket.close()
-
-def request_trigger_list(self, id: str) -> str:
-    return json.dumps({"apiName": "TITSPublicApi", "apiVersion": "1.0", "requestID": id,
-                       "messageType": "TITSTriggerListRequest"})
 
 async def main(args):
     ctx = TitsGameContext(args.connect, args.password)
-    ctx.auth = args.name
     ctx.server_task = asyncio.create_task(server_loop(ctx), name="server loop")
 
     if gui_enabled:
@@ -75,19 +40,116 @@ async def main(args):
     await ctx.exit_event.wait()
     await ctx.shutdown()
 
-
 def launch():
-    parser = get_base_parser(description="Gameless Archipelago Client, for text interfacing.")
-    parser.add_argument('--name', default=None, help="Slot Name to connect as.")
-    parser.add_argument("url", nargs="?", help="Archipelago connection url")
+    import colorama
+
+    parser = get_base_parser(description="Gameless Archipelago Client, for throwing things at VTubers.")
     args = parser.parse_args()
-
-    if args.url:
-        url = urllib.parse.urlparse(args.url)
-        args.connect = url.netloc
-        if url.username:
-            args.name = urllib.parse.unquote(url.username)
-        if url.password:
-            args.password = urllib.parse.unquote(url.password)
-
+    colorama.init()
     asyncio.run(main(args))
+    colorama.deinit()
+
+
+class TitsGameContext(CommonContext):
+    from kvui import GameManager
+    game = ""
+    httpServer_task: typing.Optional["asyncio.Task[None]"] = None
+    tags = CommonContext.tags | {"TextOnly"}
+    items_handling = 0b111  # receive all items for /received
+    want_slot_data = False  # Can't use game specific slot_data
+    command_processor = TitsCommandProcessor
+    titsPort = 42069
+    titsSocket = None
+    titsTriggers: typing.Dict[str, str]
+
+    def __init__(self, server_address, password):
+        super().__init__(server_address, password)
+        self.titsTriggers = dict()
+
+    def on_print_json(self, args: dict):
+        super(TitsGameContext, self).on_print_json(args)
+
+        """
+        if args.get("type", "") == "ItemSend" and self.slot_concerns_self(args["receiving"]) \
+            and self.slot_concerns_self(args["item"].player):
+            logger.info("Receiving item!")
+            flags = [part["flags"] for part in args if "flags" in part]
+            if flags and not all(flag & 0b001 for flag in flags):
+                logger.info("Is Progressive")
+            if flags and not all(flag & 0b010 for flag in flags):
+                logger.info("Is Useful")
+            if flags and not all(flag & 0b100 for flag in flags):
+                logger.info("Is Filler")
+        # Peel apart the args and find out the item. Check its classification and send corresponding events
+        """
+        if args.get("type", "") == "ItemSend":
+            async_start(self.send_trigger(), name="Sending Throws")
+
+    def tits_status(self):
+        if self.titsSocket is not None:
+            logger.info(f"T.I.T.S. is connected and listening on port {self.titsSocket.port}")
+            for name, trigger_id in self.titsTriggers.items():
+                logger.info(f"Found Trigger {name}: {trigger_id}")
+
+    async def connect_to_api(self):
+        try:
+            logger.info(f"Connecting to TITS on port{self.titsPort} ")
+            self.titsSocket = await websockets.connect(f"ws://localhost:{self.titsPort}/websocket", max_size=self.max_size)
+            await self.get_trigger_list()
+
+        except Exception as e:
+            print(e)
+            logger.info(f"Unable to connect. Ensure T.I.T.S. is running and API is enabled and on port {self.titsPort}")
+            self.titsSocket = None
+
+    async def get_trigger_list(self):
+        if self.titsSocket is not None:
+            await self.titsSocket.send(request_trigger_list("0"))
+            result = await self.titsSocket.recv()
+            data = json.loads(result)
+            # logger.info(result)
+            for trigger in data["data"]["triggers"]:
+                logger.info("Found Trigger: "+trigger["name"])
+                self.titsTriggers[trigger["name"]] = trigger["ID"]
+
+    async def send_trigger(self):
+        if self.titsSocket is not None:
+            await self.titsSocket.send(activate_trigger("0", self.titsTriggers["Throw a lot of things!"]))
+
+    def make_gui(self):
+        ui = super().make_gui()
+
+        class CCApp(ui):
+            def print_json(self, data):
+                text = self.json_to_kivy_parser(data)
+
+                self.log_panels["Archipelago"].on_message_markup(text)
+                self.log_panels["All"].on_message_markup(text)
+
+        return CCApp
+
+    async def server_auth(self, password_requested: bool = False):
+        if password_requested and not self.password:
+            await super(TitsGameContext, self).server_auth(password_requested)
+        await self.get_username()
+        await self.send_connect()
+
+    def on_package(self, cmd: str, args: dict):
+        super().on_package(cmd, args)
+        if cmd == "Connected":
+            self.game = self.slot_info[self.slot].game
+
+    async def disconnect(self, allow_autoreconnect: bool = False):
+        self.game = ""
+        await super().disconnect(allow_autoreconnect)
+
+def request_trigger_list(id: str) -> str:
+    return json.dumps({"apiName": "TITSPublicApi", "apiVersion": "1.0", "requestID": id,
+                       "messageType": "TITSTriggerListRequest"})
+
+def activate_trigger(id: str, trigger_id: str) -> str:
+    return json.dumps({"apiName": "TITSPublicApi", "apiVersion": "1.0",
+                       "requestID": id, "messageType": "TITSTriggerActivateRequest",
+                       "data": {
+                           "triggerID": trigger_id
+                       }})
